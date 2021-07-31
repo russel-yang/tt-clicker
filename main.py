870451237bc966392f013847ce782473a47a7a28
Russel Yang <russel.yang@take2games.com>
2021-07-30 18:54:45 -0700

    feat: simple battele and simple upgrade

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
 import pyautogui
+import time
 
-pyautogui.moveTo(1286, 647)
-pyautogui.click()
-# pyautogui.typewrite('space')
+# pyautogui.moveTo(1286, 647, 0.5)
+# pyautogui.click()
+# # pyautogui.typewrite('space')
+
+# pyautogui.moveTo(0,0, 0.5)
+def upgradeHeros():
+    heroLevelUpLocations = pyautogui.locateCenterOnScreen('assets/needles/heroUpgradable.png', confidence=0.8)
+    print(heroLevelUpLocations)
+    pyautogui.moveTo(heroLevelUpLocations.x/2, heroLevelUpLocations.y/2, 1)
+    time.sleep(1)
+    pyautogui.doubleClick()
+
+#pyautogui.displayMousePosition()
+
+def simpleBattle():
+    pyautogui.moveTo(230,600, 1)
+    pyautogui.doubleClick()
+    time.sleep(1)
+    pyautogui.click()
+    pyautogui.press('w')
+    time.sleep(2)
+    pyautogui.press('e')
+    time.sleep(2)
+    pyautogui.press('r')
+    time.sleep(2)
+    pyautogui.press('t')
+    time.sleep(2)
+    pyautogui.press('y')
+    pyautogui.press('space')
+
+#simpleBattle()
+upgradeHeros()
+
