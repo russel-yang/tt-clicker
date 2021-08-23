22eae679c9eb4733d7c081db9fc8f9d11f4ad2ca
Russel Yang <russel.yang@take2games.com>
2021-08-22 22:12:58 -0700

    fix: screenshot performace

--- main.py
+++ main.py
@@ -3,19 +3,30 @@ import time
 from PIL import ImageGrab
 import cv2
 import numpy as np
+import mss
 
-full_screen = cv2.cvtColor(np.array(ImageGrab.grab()), cv2.COLOR_RGB2BGR)
-fs_gray = cv2.cvtColor(full_screen, cv2.COLOR_BGR2GRAY)
-gear = cv2.imread("assets/needles/settings.png", cv2.IMREAD_GRAYSCALE)
-mt_result = cv2.matchTemplate(fs_gray, gear, cv2.TM_CCOEFF_NORMED, 0.9)
-min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(mt_result)
+with mss.mss() as sct:
+    full_screen = cv2.cvtColor(np.array(ImageGrab.grab()), cv2.COLOR_RGB2BGR)
+    fs_gray = cv2.cvtColor(full_screen, cv2.COLOR_BGR2GRAY)
+    gear = cv2.imread("assets/needles/settings.png", cv2.IMREAD_GRAYSCALE)
+    mt_result = cv2.matchTemplate(fs_gray, gear, cv2.TM_CCOEFF_NORMED, 0.9)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(mt_result)
 
-x, y = max_loc
-tt2 = ImageGrab.grab((x, y, x+1204, y+2150))
-game = cv2.cvtColor(np.array(tt2), cv2.COLOR_RGB2BGR)
+    x, y = max_loc
+    last_time = time.time()
+    monitor = {"top": int(y/2), "left": int(x/2), "width": 600, "height": 1075}
 
-cv2.imshow('screen', game)
-cv2.waitKey(0)
+    while True:
+        tt2 = sct.grab(monitor)
+        game = np.array(tt2)
+
+        cv2.imshow('screen', game)
+        #print('fps ={}'.format(1/(time.time()-last_time)))
+        last_time = time.time()
+        if cv2.waitKey(25) & 0xFF == ord("q"):
+            cv2.destroyAllWindows()
+            break
+
 
 # pyautogui.moveTo(1286, 647, 0.5)
 # pyautogui.click()
